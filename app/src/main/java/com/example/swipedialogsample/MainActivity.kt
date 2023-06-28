9a067125538e4b6e8ecd0aeaa34f3197f37b748d
Tsuyoshi Ogihara <tsuyoshi.ogihara@voicy.jp>
2023-06-28 13:35:31 +0900

    createSwipeDialogComposeSample

---
+++ app/src/main/java/com/example/swipedialogsample/MainActivity.kt
@@ -0,0 +1,160 @@
+package com.example.swipedialogsample
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import com.example.swipedialogsample.ui.theme.SwipeDialogSampleTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SwipeDialogSampleTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val openDialog = remember { mutableStateOf(false) }
+
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Button(
+                            onClick = {
+                                openDialog.value = true
+                            }
+                        ) {
+                            Text(text = "Open Dialog")
+                        }
+                    }
+
+                    if (openDialog.value) {
+                        SwipeDialog() {
+                            openDialog.value = false
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalFoundationApi::class)
+@Composable
+fun SwipeDialog(closeDialog: () -> Unit) {
+    var buttonTapScrollToPage by remember { mutableStateOf(false) }
+    var scrollToPageInt by remember { mutableStateOf(0) }
+    val pageCount = 6
+
+    Dialog(
+        onDismissRequest = {}
+    ) {
+        Surface(shape = RoundedCornerShape(30.dp), color = Color.Transparent) {
+            val pagerState = rememberPagerState(initialPage = 0)
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Surface(shape = RoundedCornerShape(30.dp)) {
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(0.dp, 10.dp)
+                    ) {
+                        HorizontalPager(state = pagerState, pageCount = pageCount) { page ->
+                            Column(
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                            ) {
+                                Text(
+                                    text = "Page: $page",
+                                    textAlign = TextAlign.Center,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .padding(0.dp, 30.dp)
+                                )
+                                Button(
+                                    onClick = {
+                                        if (page == pageCount - 1) {
+                                            closeDialog()
+                                        } else {
+                                            scrollToPageInt = page + 1
+                                            buttonTapScrollToPage = true
+                                        }
+                                    }
+                                ) {
+                                    Text(text = "Button $page")
+                                }
+                            }
+                        }
+                        LaunchedEffect(buttonTapScrollToPage) {
+                            if (buttonTapScrollToPage) {
+                                pagerState.animateScrollToPage(scrollToPageInt)
+                                buttonTapScrollToPage = false
+                            }
+                        }
+                    }
+                }
+                Row(
+                    Modifier
+                        .height(30.dp)
+                        .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.Bottom
+                ) {
+                    repeat(pageCount) { iteration ->
+                        val color =
+                            if (pagerState.currentPage == iteration) Color.DarkGray else Color.LightGray
+                        Box(
+                            modifier = Modifier
+                                .padding(2.dp)
+                                .clip(CircleShape)
+                                .background(color)
+                                .size(5.dp)
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
